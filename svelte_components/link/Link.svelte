b5200dd0544ef2ba60ce69e0c03584e3515e6eb2
Michel Grautstueck <michel.grautstueck@nzz.ch>
2022-04-13 16:29:24 +0200

    Add svelte link component

---
+++ svelte_components/link/Link.svelte
@@ -0,0 +1,25 @@
+<script>
+  // Component for handling internal & external links in all environments (Desktop, Mobile, Mobile-App)
+  export let url;
+  export let linkTarget = "";
+  export let innerHtml;
+  export let elementType = "div";
+
+  function openUrl(url) {
+    if (window.NZZ && vamp.navigateToArticle) {
+      vamp.navigateToArticle(url.substr(url.lastIndexOf("Id.") + 1));
+    } else {
+      window.open(url, linkTarget).focus();
+    }
+  }
+</script>
+
+{#if elementType === "div"}
+  <div on:click={() => openUrl(url)} class="nzz-qcc-link">
+    {@html innerHtml}
+  </div>
+{:else if elementType === "span"}
+  <span on:click={() => openUrl(url)} class="nzz-qcc-link">
+    {@html innerHtml}
+  </span>
+{/if}
