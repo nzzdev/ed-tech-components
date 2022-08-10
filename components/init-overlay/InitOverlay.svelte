1dacb03d0f0e262fe12921a3f6f6078bcd3acd12
Michel Grautstueck <michel.grautstueck@nzz.ch>
2022-08-10 12:45:35 +0200

    Add raw init overlay

---
+++ components/init-overlay/InitOverlay.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  let initialized = false;
+
+  export let initCallback: () => void = () => {};
+
+  function initialize() {
+    initialized = true;
+
+    initCallback();
+  }
+</script>
+
+<div on:click|once={initialize} class="etsr__init-overlay">
+  {#if $$slots.overlay && $$slots.content}
+    {#if !initialized}
+      <slot name="overlay" />
+    {/if}
+
+    {#if initialized}
+      <slot name="content" />
+    {/if}
+  {:else}
+    Missing named slots 'overlay' & 'content'
+  {/if}
+</div>
